03f6756e93ec3f60fa1a15f7af64232c2844db37
Nick Speidell <nspeidell@gmail.com>
2023-02-16 20:20:28 -0700

    revised object3lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -5,8 +5,8 @@ public class Objective3Lab3 {
 		int third = 1 + (int)(Math.random() * 6);
         int sum = first + second + third;
 
-		System.out.println(first);
-		System.out.println(second);
+		System.out.println(+ first + "+" + second + "+" + third + "=" sum) 
+		System.out.println(second); 
 		System.out.println(third);
 		System.out.println(sum);
 		 
