483eea12cf709e86bf7a8942a7908a77cf8df2d5
Nick Speidell <nspeidell@gmail.com>
2023-02-24 11:40:06 -0700

    objectives 6 and 7

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,11 @@
+public class Objective7Lab2 {
+	public static void main(String[] args) {
+		int counter = 0;
+
+		while(counter <= 20) {
+				System.out.println("Number is " + counter);
+
+			counter = counter + 1;
+		}
+	}
+}
---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+public static void main(String[] args) {
+    int counter = 1;
+    int sum = 0;
+
+    while(counter <= 20) {
+	 System.out.println(counter);
+    	counter = counter +1;
+    	sum = sum + counter;
+    }
+System.out.println("Sum is " + sum);
+}
+}
